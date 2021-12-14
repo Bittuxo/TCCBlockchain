4caa3a0a6b05fa2e3da9b6db794809409bdcdf2b
Vitor <68917166+Bittuxo@users.noreply.github.com>
2021-12-13 21:50:08 -0300

    Ajuste final TCC

--- contracts/TCC.sol
+++ contracts/TCC.sol
@@ -8,11 +8,13 @@ pragma solidity >=0.7.0 <0.9.0;
         string  nEM; //nome empresa
         string  tPR; //tempo previsto
         string  rEQ; //requisições do software
-        
-        function setCETR(string memory NomeCliente, string memory NomeEmpresa, string memory TempoPrevisto, string memory Requisitos) public {
+        string  sTs; //status do contrato
+
+        function setCETR(string memory NomeCliente, string memory NomeEmpresa, string memory TempoPrevisto, string memory Requisitos, string memory Status) public {
             nCL = NomeCliente;
             nEM = NomeEmpresa;
             tPR = TempoPrevisto;
             rEQ = Requisitos;
+            sTs = Status;
         }  
     }
